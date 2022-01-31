670524d4a8ff5e061a74bb28ca3e71310adf47a6
Aymane Boukrouh <49211158+AymaneBoukrouh@users.noreply.github.com>
2022-01-31 06:16:56 +0100

    update run.py
    Moved the app and mainWindow from __init__.py file to run.py in order to
    prevent the window from opening when importing classes to use in unit
    testing

--- pichess/__init__.py
+++
@@ -1,8 +0,0 @@
-from pichess.mainwindow import MainWindow
-
-from PyQt6.QtWidgets import QApplication
-import sys
-
-
-app = QApplication(sys.argv)
-mainWindow = MainWindow()
--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
 
-from pichess import app
+from pichess.mainwindow import MainWindow
+from PyQt6.QtWidgets import QApplication
 import sys
 
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
     sys.exit(app.exec())
