e2da8762bb97c6d4519943d3b2fc6c3bf9a4b752
Aymane Boukrouh <49211158+AymaneBoukrouh@users.noreply.github.com>
2022-02-05 22:13:33 +0100

    :rotating_light: test (engine): fix rook pseudolegal_moves

--- tests/engine/test_rook.py
+++ tests/engine/test_rook.py
@@ -38,12 +38,12 @@ class TestRook(TestCase):
             ['f1', True, fen['3'], {'a1', 'b1', 'c1', 'd1', 'e1', 'g1', 'h1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7'}],
             ['h1', True, fen['5'], set()],
             ['a1', True, fen['7'], {'b1', 'c1'}],
-            ['f3', True, fen['9'], {'a3', 'b3', 'c3', 'd3', 'e3', 'f2'}],
-            ['a3', False, fen['2'], {'a3', 'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'a1', 'a2', 'a4', 'a5'}],
+            ['f3', True, fen['9'], {'a3', 'b3', 'c3', 'd3', 'e3', 'g3', 'f2'}],
+            ['a3', False, fen['2'], {'b3', 'c3', 'd3', 'e3', 'f3', 'g3', 'a1', 'a2', 'a4', 'a5'}],
             ['h8', False, fen['4'], {'h5', 'h6', 'h7'}],
-            ['c8', False, fen['6'], {'a8', 'b8', 'd3', 'e8', 'c5', 'c6', 'c7'}],
-            ['a8', False, fen['8'], {'b7', 'c8', 'd8'}],
-            ['g8', False, fen['10'], {'h7'}]
+            ['c8', False, fen['6'], {'a8', 'b8', 'd8', 'e8', 'c5', 'c6', 'c7'}],
+            ['a8', False, fen['8'], {'b8', 'c8', 'd8'}],
+            ['h8', False, fen['10'], {'h7'}]
         ]
 
         for coordinates, color, fen, pseudolegal_coordinates in data:
